3f49c7cb08d50f3c07a95bae2509da9e845cfbf8
Fieava Chen <Fieava@gmail.com>
2016-04-07 22:46:51 +0800

    * navi bar

--- resources/views/frame/frame.blade.php
+++ resources/views/frame/frame.blade.php
@@ -17,7 +17,12 @@
 	<div id="avatar"><img src="https://avatars2.githubusercontent.com/u/7803638?v=3&s=40"/></div>
 	<span id="user_name" title="Fieava">Fieava</span>
 	<nav id="main_nav">
-
+		<div id="nav_project" class="nav_item_now nav_item"><i class="fa fa-archive fa-2x"></i></div>
+		<div id="nav_model" class="nav_item"><i class="fa fa-cogs fa-2x"></i></div>
+		<div id="nav_task" class="nav_item"><i class="fa fa-flag fa-2x"></i></div>
+		<div id="nav_setting" class="nav_item"><i class="fa fa-sliders fa-2x"></i></div>
+		<div id="nav_message" class="nav_item"><i class="fa fa-comments fa-2x"></i></div>
+		<div id="nav_help" class="nav_item nav_item_end"><i class="fa fa-question-circle fa-2x"></i></div>
 	</nav>
 </header>
 <nav id="sub_nav">
