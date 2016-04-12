ba36664feecf0f79873804b88b6c88fb973f0bb3
Fieava Chen <Fieava@gmail.com>
2016-04-12 22:08:39 +0800

    + sub_nav.models

--- app/Http/Controllers/SubNavController.php
+++ app/Http/Controllers/SubNavController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Models;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Routing\Controller as BaseController;
 
@@ -26,7 +27,45 @@ class SubNavController extends BaseController {
 
 	function model() {
 		$projects = Auth::user()->projects()->where('status', '!=', 0)->Where('status', '!=', 5)->get();
-		$models = Auth::user()->models()->where('status', '!=', 0)->Where('status', '!=', 5)->get();
-		return view('sub_nav.model')->with('model', $models);
+		foreach ($projects as $project) {
+			$project_list[] = $project->id;
+		}
+		if (is_array($project_list)) {
+			$models = Models::where('project', $project_list)->where('status', '!=', 0)->Where('status', '!=', 5)->Where('parent', NULL)->get()->toArray();
+			$models = $this::get_sub_models($models);
+		} else {
+			$models = [];
+		}
+
+		return view('sub_nav.model')->with('models', json_encode($models));
+	}
+
+	function get_sub_models($models) {
+		if (is_array($models) && empty($models['id'])) {
+			foreach ($models as $key => $model) {
+				$models[$key]['sub_models'] = $this->get_sub_models_from_DB($model['id']);
+				if (!is_null($models[$key]['sub_models'])) {
+					foreach ($models[$key]['sub_models'] as $ed_key => $ed_model) {
+						$models[$key]['sub_models'][$ed_key] = $this->get_sub_models($ed_model);
+					}
+				} else {
+					return NULL;
+				}
+			}
+		} else {
+			$models['sub_models'] = $this->get_sub_models_from_DB($models['id']);
+			if (!is_null($models['sub_models'])) {
+				foreach ($models['sub_models'] as $ed_key => $ed_model) {
+					$models['sub_models'][$ed_key] = $this->get_sub_models($ed_model);
+				}
+			} else {
+				return NULL;
+			}
+		}
+		return $models;
+	}
+
+	function get_sub_models_from_DB($id) {
+		return Models::where('parent', $id)->where('status', '!=', 0)->Where('status', '!=', 5)->get()->toArray();
 	}
 }
--- resources/views/sub_nav/model.blade.php
+++ resources/views/sub_nav/model.blade.php
@@ -1,8 +1,9 @@
 <div id="sub_nav_container">
 	<ul id="sub_nav_list">
-		@foreach($models as $model)
-			<li onclick="load_content(this, 'projects/info/{{ $model['id'] }}')"><i class="fa fa-archive"></i> {{ $model['development_name'] }}<span
-						class="project_version">{{ $model['version'] }}</span></li>
-		@endforeach
 	</ul>
+	<div id="Sub_nav_list_template" style="display: none"></div>
+	<script>
+		console.log({!! $models !!});
+		Sub_nav.show_models({!! $models !!});
+	</script>
 </div>
